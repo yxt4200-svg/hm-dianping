ed6aef2e0a413650e5406c6880aa7280b373eef7
yxt4200-svg <yxt4200@gmail.com>
2026-02-08 15:40:20 +0800

    分布式锁13：解决锁误删问题(√)

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -4,16 +4,21 @@ import cn.hutool.core.util.BooleanUtil;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
+import cn.hutool.core.util.IdUtil;
 
 /**
  * 一人一单
  */
 public class SimpleRedisLock implements ILock  {
 
-    private String name;
+    private String name; // 锁的名字
     private StringRedisTemplate stringRedisTemplate;
-    private static final String KEY_PREFIX = "lock:";
+    private static final String KEY_PREFIX = "lock:"; // 锁的前缀
+
+    // 1.服务器专属门牌（UUID）：解决多JVM线程号重复问题，一台服务器一个唯一门牌
+    private static final String ID_PREFIX = IdUtil.simpleUUID() + "-";
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
@@ -22,19 +27,28 @@ public class SimpleRedisLock implements ILock  {
 
     @Override
     public boolean tryLock(long timeoutSec) {
-        // 获取线程表示，value可以是线程名也可以是线程号，只要能标识是谁抢的锁就行
-        long threadId = Thread.currentThread().getId();
-        // 获取锁
+        // value可以是线程名也可以是线程号，只要能标识是谁抢的锁就行
+        // 2. 生成当前线程的唯一标识：服务器门牌 + 线程号（比如abc123-101）
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        // 3.获取锁
         // setIfAbsent：还没人占锁，我就占，并且给锁设个过期时间；如果已经被人占了，我就抢不到
         Boolean success = stringRedisTemplate.opsForValue()
-                .setIfAbsent(KEY_PREFIX + name, threadId + "", timeoutSec, TimeUnit.SECONDS);
+                .setIfAbsent(KEY_PREFIX + name, threadId , timeoutSec, TimeUnit.SECONDS);
         // return success; 错误，自动拆箱若为null会空指针异常
+        // 4.安全返回抢锁结果
         return Boolean.TRUE.equals(success);
     }
 
     @Override
     public void unlock() {
-        // 释放锁
-        stringRedisTemplate.delete(KEY_PREFIX + name);
+        // 1.生成自己的线程唯一标识
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        // 2. 去Redis查账本：查这把锁当前的占用者是谁（存的是抢锁时的线程标识
+        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+        // 3.核对身份
+        if(threadId.equals(id)) {
+            // 释放锁
+            stringRedisTemplate.delete(KEY_PREFIX + name);
+        }
     }
 }
